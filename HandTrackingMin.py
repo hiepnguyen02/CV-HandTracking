27f4ae4952d91960eb6d37545141ac3ceb0ef363
hiepnguyen02 <hiepnguyen.uet.vnu@gmail.com>
2023-01-13 23:28:37 +0700

    landmarks

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -3,8 +3,28 @@ import mediapipe as mp
 import time
 
 cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+pTime = 0
+cTime = 0
 
 while True:
     success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    # print(results.multi_hand_landmarks)
+    if results.multi_hand_landmarks:
+        for landLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, landLms, mpHands.HAND_CONNECTIONS,
+                                  mpDraw.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                  mpDraw.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2))
+
+    cTime = time.time()
+
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
